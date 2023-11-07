3910810f1bec0311f8e7f399df818b35d2ef9fdc
Zaid <zaid.sheikh@idenfo.com>
2023-11-07 16:31:05 +0500

    Initial Commit

---
+++ NovelNest/NovelNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  NovelNestApp.swift
+//  NovelNest
+//
+//  Created by Zaid on 07/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct NovelNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
